087c9241f11e44977174d16d8cbde6cfd4ff5180
1999ADEK <b06201018@ntu.edu.tw>
2020-07-17 11:36:00 +0800

    Create model with ASPP module

---
+++ model_ASPP.py
@@ -0,0 +1,67 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.utils import _pair
+from module import Conv2d_modified, ConvLSTM, create_featex, NestedWindow, GlobalStd2D, ASPP
+import load_weights
+import argparse
+
+##替代 create_manTraNet_model功能
+class ManTraNet(nn.Module):
+    def __init__(self, Featex, pool_size_list=[7,15,31], is_dynamic_shape=True, apply_normalization=True):
+        super().__init__()
+        self.Featex = Featex
+        self.pool_size_list = pool_size_list
+        self.is_dynamic_shape = is_dynamic_shape
+        self.apply_normalization = apply_normalization
+
+        #layers
+        self.outlierTrans = Conv2d_modified.Conv2d_samepadding(256, 64, 1, padding='SAME')  #在optimizer那邊要做apply_normalization
+        self.pred = Conv2d_modified.Conv2d_samepadding(8, 1, 7, padding="SAME", bias=True)  
+        self.bnorm = nn.BatchNorm2d(64, affine=False, eps=1e-3)
+        self.nestedAvgFeatex = NestedWindow.NestedWindowAverageFeatExtrator(window_size_list= self.pool_size_list, 
+                                                                               output_mode='5d',
+                                                                               minus_original=True) 
+        self.glbStd = GlobalStd2D.GlobalStd2D(64)   #input: number of features
+        self.cLSTM = ConvLSTM.ConvLSTM(input_dim = 64, hidden_dim = 8, kernel_size = (7, 7), num_layers = 1, batch_first = True, bias = True, return_all_layers = False)
+        self.sigmoid = nn.Sigmoid()
+        self.featex = None
+        self.clstm = None
+        self.aspp = ASPP.ASPP(2)
+    def forward(self,x):
+        rf = self.Featex(x) 
+        pred_out = self.aspp(rf)
+
+        return pred_out
+
+def create_model(IMC_model_idx, freeze_featex, window_size_list=[7,15,31], OPTS_aspp = False):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    type_idx = IMC_model_idx if IMC_model_idx < 4 else 2
+    Featex = create_featex.Featex_vgg16_base(type_idx)
+    if freeze_featex:
+        print("INFO: freeze feature extraction part, trainable=False")
+        #Featex.trainable = False ##它沒有trainable這個選項阿？？？  
+    else:
+        print ("INFO: unfreeze feature extraction part, trainable=True")
+    """
+    if len(window_size_list) == 4:
+        for ly in Featex.layers[:5]:
+            ly.trainable = False  ##也沒有這個選項
+            print("INFO: freeze", ly.name)
+    """
+    model = ManTraNet(Featex, pool_size_list=window_size_list, is_dynamic_shape=True, apply_normalization=True, OPTS_aspp=OPTS_aspp)
+    return model
+
+
+def model_load_weights(weight_path, model):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = load_weights.load_weights(weight_path, model)    
+    model.to(device)
+
+    return model
+
+
+
+
+
